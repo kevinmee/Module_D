c1778f8a172e15b7b3063e2f4e4d6899b2051f01
Tucker Tibbetts <ctibbetts@digitalresultsgroup.com>
2014-12-01 23:04:37 -0500

    start on SQL files for populating DB

---
+++ sql/known_turtles.sql
@@ -0,0 +1,7 @@
+CREATE TABLE known_turtles(name VARCHAR(20), species VARCHAR(20), age INT, location VARCHAR(20));
+
+INSERT INTO known_turtles VALUES ('Donatello', 'Teenage Mutant Ninja',  15, 'Sewers')
+INSERT INTO known_turtles VALUES ('Leonardo', 'Teenage Mutant Ninja',  15, 'Sewers')
+INSERT INTO known_turtles VALUES ('Raphael', 'Teenage Mutant Ninja',  15, 'Sewers')
+INSERT INTO known_turtles VALUES ('Michelangelo', 'Teenage Mutant Ninja',  15, 'Sewers')
+
