41343cadc3208249b39764f5fc8b5ec0d3ac76b5
kevinmee <kevin.mee@gmail.com>
2014-12-02 00:28:03 -0500

    turtle_species VARCHAR changed to 30

--- sql/turtle_species.sql
+++ sql/turtle_species.sql
@@ -1,7 +1,7 @@
-CREATE TABLE turtle_species(species VARCHAR(30), avg_age INT, avg_weight INT)
+CREATE TABLE turtle_species(species VARCHAR(30), avg_age INT, avg_weight INT);
 
-INSERT INTO turtle_species VALUES ('Teenage Mutant Ninja Turtle', 80, 88)
-INSERT INTO turtle_species VALUES ('Snapping', 30, 25)
-INSERT INTO turtle_species VALUES ('Diamondback Terrapin', 45, 50)
-INSERT INTO turtle_species VALUES ('Red Ear', 60, 34)
-INSERT INTO turtle_species VALUES ('Yellow Bellied', 20, 30)
+INSERT INTO turtle_species VALUES ('Teenage Mutant Ninja Turtle', 80, 88);
+INSERT INTO turtle_species VALUES ('Snapping', 30, 25);
+INSERT INTO turtle_species VALUES ('Diamondback Terrapin', 45, 50);
+INSERT INTO turtle_species VALUES ('Red Ear', 60, 34);
+INSERT INTO turtle_species VALUES ('Yellow Bellied', 20, 30);
