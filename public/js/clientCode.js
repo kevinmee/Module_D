a75bd7dc32a3805d0647339339dba80394660f87
Tucker Tibbetts <ctibbetts@digitalresultsgroup.com>
2014-12-02 01:15:48 -0500

    no message

--- public/js/clientCode.js
+++ public/js/clientCode.js
@@ -4,19 +4,21 @@ $(function() {
     var speciesData = {
         chart: {
             renderTo: 'speciesData',
-            type: 'bar'
+            type: 'bar',
+            marginLeft: 50,
+            marginRight: 50
         },
         title: {
             text: 'Average Weight of Turtles by Species'
         },
         xAxis: {
             title: {
+                text: 'Weight in Pounds'
+            }
+        },
+        yAxis: {
+            title: {
                 text: 'Species of Turtle'
-            },
-            yAxis: {
-                title: {
-                    text: 'Weight in Pounds'
-                }
             }
         },
         series: []
@@ -25,6 +27,25 @@ $(function() {
     $.getJSON('/speciesWeight', function(data) {
         speciesData.series = data;
         var speciesWeightChart = new Highcharts.Chart(speciesData);
+    });
+
+    $('#weightGraph').click(function() {
+        speciesData.chart.type = "bar";
+
+        $.getJSON('/speciesWeight', function(data) {
+            speciesData.series = data;
+            var speciesWeightChart = new Highcharts.Chart(speciesData);
+        });
+    });
+
+    // Change Snack Consumption from bar to line
+    $('#ageGraph').click(function() {
+        speciesData.chart.type = "bar";
+
+        $.getJSON('/speciesAge', function(data) {
+            speciesData.series = data;
+            var speciesWeightChart = new Highcharts.Chart(speciesData);
+        });
     });
 
 
