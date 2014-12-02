a9f07a25e954edb3b3161002583ad30964d2aebb
kevinmee <kevin.mee@gmail.com>
2014-12-02 00:25:24 -0500

    turtle_species VARCHAR changed to 30

--- sql/known_turtles.sql
+++ sql/known_turtles.sql
@@ -1,4 +1,4 @@
-CREATE TABLE known_turtles(name VARCHAR(20), species VARCHAR(20), age INT, location VARCHAR(20));
+CREATE TABLE known_turtles(name VARCHAR(20), species VARCHAR(30), age INT, location VARCHAR(20));
 
 INSERT INTO known_turtles VALUES ('Donatello', 'Teenage Mutant Ninja',  15, 'Sewers')
 INSERT INTO known_turtles VALUES ('Leonardo', 'Teenage Mutant Ninja',  15, 'Sewers')
